c48cb67df5a468eb57b6b76869be8d0505557b7c
Daniel Sonne Lehnberg <doddy@doddy.se>
2020-12-26 20:41:27 +0100

    Add debugging capabilities, improve cli usage somewhat

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,27 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    let file = if env::args().count() == 2 {
-        println!("File: {}", env::args().nth(1).unwrap());
-        env::args().nth(1).unwrap()
+    let count = env::args().count();
+    let file = if count >= 2 {
+        let file_arg = env::args().nth(1).unwrap();
+        println!("File: {}", file_arg);
+        file_arg
     } else {
-        panic!("Please enter a file")
+        panic!("Please enter a file.")
     };
 
-    let img = image::open(&Path::new(&file)).unwrap();
+    let file_name = file.split(" ").find(|x| x != &"--file").unwrap();
+    let img = image::open(&Path::new(&file_name)).unwrap();
 
     let result = img.pixels().into_iter().fold(0, |acc, (_x, _y, pixel)| {
-        acc + pixel[0] as u128 * pixel[1] as u128 * pixel[2] as u128 * pixel[3] as u128
+        acc + (pixel[0] as u32) + (pixel[1] as u32) + (pixel[2] as u32) + (pixel[3] as u32)
     });
 
     println!("Result: {}", result);
     generate_output(result);
 }
 
-fn generate_output(res: u128) {
+fn generate_output(res: u32) {
     if !Path::new("output/").exists() {
         fs::create_dir("output/").unwrap();
     }
