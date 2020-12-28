8306a9d03baee44c0a6a6f418976f260fbad5925
Daniel Sonne Lehnberg <doddy@doddy.se>
2020-12-28 16:19:02 +0100

    Use standard library for calculating hash

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,30 @@
-extern crate image;
-
-use image::GenericImageView;
-use std::env;
-use std::fs;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 use std::path::Path;
 
-fn main() {
-    let count = env::args().count();
-    let file = if count >= 2 {
-        let file_arg = env::args().nth(1).unwrap();
-        println!("File: {}", file_arg);
-        file_arg
-    } else {
-        panic!("Please enter a file.")
-    };
+fn main() -> Result<(), std::io::Error> {
+    println!("Please enter a file.");
 
-    let file_name = file.split(" ").find(|x| x != &"--file").unwrap();
-    let img = image::open(&Path::new(&file_name)).unwrap();
+    let mut input_string = String::new();
+    std::io::stdin().read_line(&mut input_string)?;
 
-    let result = img.pixels().into_iter().fold(0, |acc, (_x, _y, pixel)| {
-        acc + (pixel[0] as u32) + (pixel[1] as u32) + (pixel[2] as u32) + (pixel[3] as u32)
-    });
+    let tmp = input_string.trim();
+    let path_input = Path::new(&tmp);
+    let file_content = std::fs::read(path_input.as_os_str());
 
-    println!("Result: {}", result);
-    generate_output(result);
-}
+    match file_content {
+        Ok(res) => {
+            let mut hasher = DefaultHasher::new();
+            res.hash(&mut hasher);
+            let finished_hash = hasher.finish();
 
-fn generate_output(res: u32) {
-    if !Path::new("output/").exists() {
-        fs::create_dir("output/").unwrap();
+            println!("Result:");
+            println!("{}", finished_hash);
+            return Ok(());
+        }
+        Err(err) => {
+            println!("Error: {}", err);
+            return Err(err);
+        }
     }
-    let mut out_file: String = String::from("output/");
-    out_file.push_str(&res.to_string());
-    out_file.push_str(".txt");
-    fs::write(&out_file, res.to_string()).unwrap();
 }
